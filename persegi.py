bf04c2e1764e4ced4e12ed027b0e9796c67d254e
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-03-26 09:43:40 +0700

    membuat file persegi.py
    menambahkan rumus perhitungan mudah untuk menemukan luas persegi

---
+++ persegi.py
@@ -0,0 +1,5 @@
+#Luas Persegi
+print(=====Masukan Angka Panjang/Lebar nya Berapa=====)
+angka = int(input("Panjang dan Lebar persegi adalah ="))
+rumus = angka*angka
+print("Maka luas persegi adalah =", rumus)
