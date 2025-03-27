a2cbacb88dcdea5c2a648024b3371427807a77b2
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-03-27 07:28:45 +0700

    Update makan.py

--- makan.py
+++ makan.py
@@ -2,7 +2,7 @@ print ("===== Jam Makan Prince =====")
 waktu = int(input("Masukan jam berapa = ")) #masukan jam berapa hari ini
 if waktu == 12:
   print ("waktunya makan")
-  else :
+else:
   print ("sabar tetap waspada")
 print(waktu)
 
