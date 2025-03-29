f7d6925bac65bcc4430cc1a5ef9074b3da4bcc27
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-03-29 19:50:33 +0700

    Update hari.py

--- hari.py
+++ hari.py
@@ -1,9 +1,11 @@
 #date and time (latihan)
 
 import datetime as dt
+
 print("silahkan masukan tanggal bulan\n dan tahun lahir anda\n")
 tanggal = int(input("Tanggal \t:"))
 bulan = int(input("Bulan \t\t:"))
 tahun = int(input("Tahun \t\t:"))
 
 tanggal_lahir = dt.date(tahun,bulan,tanggal)
+print(f"Tanggal lahir anda adalah : {tanggal_lahir}")
