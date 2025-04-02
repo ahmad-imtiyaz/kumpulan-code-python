647098e81d7c8bf6f87f1761f400c0533c292a06
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-04-02 22:02:29 +0700

    Update damage.py

--- damage.py
+++ damage.py
@@ -1,6 +1,9 @@
-#Berikan damage ke monster
-print("Kamu akan menghadapi monster pertama")
+import random
+
+print("Kamu akan menghadapi monster pertama!")
+print("Pilih skill yang akan digunakan:")
 print("1. Skill Absolute Slash")
 print("2. Skill Deduction")
-print("3. Skill Double Dager")
-damage = int(input("mau pakai skill berapa = "))
+print("3. Skill Double Dagger")
+
+damage = int(input("Mau pakai skill berapa = "))
