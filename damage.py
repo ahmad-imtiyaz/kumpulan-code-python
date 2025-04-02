ec0f15c6c9e5a778553af61cf7326627db9e2d37
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-04-02 21:56:12 +0700

    Update damage.py

--- damage.py
+++ damage.py
@@ -1,4 +1,5 @@
 #Berikan damage ke monster
 print("Kamu akan menghadapi monster pertama")
 print("1. Skill Absolute Slash")
+print("2. Skill Deduction")
 damage = int(input("mau pakai skill berapa = "))
