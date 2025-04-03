3188cc11f8203fb2d172aebc714579abee9e4613
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-04-03 19:36:50 +0700

    Update suhu.py

--- suhu.py
+++ suhu.py
@@ -2,4 +2,5 @@
 suhu = int(input("masukan suhu celcius yang akan dirubah menjadi farenhait = "))
 print("===== Suhu Celcius =====")
 print(f"Suhu celcius adalah = {suhu} ")
+suhu = (suhu * 9/5) +32
 print(f"Maka jika diuabah menjadi farenhait adalah = {far} ")
