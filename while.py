1617075fb14d872ec4486fb17a821ce0cb981691
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-04-10 21:11:48 +0700

    menambahkan file baru

---
+++ while.py
@@ -0,0 +1,17 @@
+# While loop
+
+# while kondisi:
+#     aksi ini
+#     aksi itu
+
+# akhir dari progam
+
+angka = 0
+print(f"angka sekarang -> {angka}")
+
+while angka < 10:
+    angka += 2
+    print(f"angka sekarang -> {angka}")
+    print("ulang lagi keatas")
+
+print("progam telah berakhir")
