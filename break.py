c84ff14d6f6e18d8ddaa207d2d85f40831bdb352
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-04-12 21:31:16 +0700

    apanih gua nambahin stu file

---
+++ break.py
@@ -0,0 +1,32 @@
+# Break
+
+angka = 0
+
+while angka < 10:
+    angka += 2
+    print(f"angka sekarang -> {angka}")
+
+    if angka == 6:
+        print("berhenti")
+        break
+
+    print("lanjut")
+
+print("progam selesai")
+
+data_int = int(input("Hitung nyampe = "))
+
+angka = 0
+
+while True:
+    angka += 1
+    print(f"angka sekarang -> {angka}")
+
+    if angka == data_int:
+        print(f"Sutop")
+        break
+
+    print("lanjut")
+
+print("akhir progam")
+
