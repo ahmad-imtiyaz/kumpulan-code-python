3c06a9d461111a28ff5e304d8ee35d302a203ebb
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-04-15 22:39:43 +0700

    Update list.py

--- list.py
+++ list.py
@@ -34,6 +34,6 @@ print(list_pake_for_if)
 list_pake_for_if = [i for i in range(0,10) if i%2 == 0]
 print(list_pake_for_if)
 
-# membuat list menampilkan Bilangan Ganjil
+# Membuat list menampilkan Bilangan Ganjil
 list_pake_for_if = [i for i in range(0,10) if i%2 != 0]
 print(list_pake_for_if)
