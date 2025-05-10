f9cb70fdcd5eca965c22ae63b9c1ae6e8e4375b9
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-05-10 22:33:13 +0700

    menambahkan code piramida huruf

---
+++ gpt2.py
@@ -0,0 +1,14 @@
+# Function to create a pyramid of letters
+def letter_pyramid(custom_letter='*', height=5):
+    for i in range(1, height + 1):
+        # Print spaces for alignment
+        print(' ' * (height - i), end='')
+        # Print the custom letter in pyramid shape
+        print((custom_letter + ' ') * i)
+
+# Example usage
+if __name__ == "__main__":
+    # Customize the letter and height here
+    letter = input("Enter the letter for the pyramid: ")
+    height = int(input("Enter the height of the pyramid: "))
+    letter_pyramid(letter, height)
