89d09d419041ddc5313558cb918a66715b1e7af7
Ahmad Imtiyaz <imtiyaznajih8@gmail.com>
2025-05-12 18:29:36 +0700

    Update Armageddon

--- damage.py
+++ damage.py
@@ -3,7 +3,7 @@ import random
 print("Kamu akan menghadapi monster pertama!")
 print("Pilih skill yang akan digunakan:")
 print("1. Skill Absolute Slash")
-print("2. Skill Deduction")
+print("2. Skill Armageddon")
 print("3. Skill Double Dagger")
 
 damage = int(input("Mau pakai skill berapa = "))
